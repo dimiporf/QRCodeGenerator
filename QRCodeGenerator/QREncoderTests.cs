a5b057a375314cbe16ee83a4a9a64a718d264cb1
Dimitrios Porfiropoulos <dimiporf@live.com>
2023-12-26 18:54:47 +0100

    Add project files.

---
+++ QRCodeGenerator/QREncoderTests.cs
@@ -0,0 +1,42 @@
+﻿using QRImplementation;
+
+namespace QRCodeGenerator
+{
+    public class QREncoderTests
+    {
+        [Fact]
+        public void TestNumericMode()
+        {
+            QREncoder encoder = new QREncoder();
+            Assert.Equal(QREncodingMode.Numeric, encoder.DetermineEncodingMode("123456"));
+        }
+
+        [Fact]
+        public void TestAlphanumericMode()
+        {
+            QREncoder encoder = new QREncoder();
+            Assert.Equal(QREncodingMode.Alphanumeric, encoder.DetermineEncodingMode("ABC123$%"));
+        }
+
+        [Fact]
+        public void TestByteMode()
+        {
+            QREncoder encoder = new QREncoder();
+            Assert.Equal(QREncodingMode.Byte, encoder.DetermineEncodingMode("Hello"));
+        }
+
+        [Fact]
+        public void TestKanjiMode()
+        {
+            QREncoder encoder = new QREncoder();
+            Assert.Equal(QREncodingMode.Kanji, encoder.DetermineEncodingMode("漢字"));
+        }
+
+        [Fact]
+        public void TestUnknownMode()
+        {
+            QREncoder encoder = new QREncoder();
+            Assert.Equal(QREncodingMode.Unknown, encoder.DetermineEncodingMode("!@#"));
+        }
+    }
+}
---
+++ QRImplementation/QREncoder.cs
@@ -0,0 +1,58 @@
+﻿namespace QRImplementation
+{
+    public enum QREncodingMode
+    {
+        Numeric,
+        Alphanumeric,
+        Byte,
+        Kanji,
+        Unknown
+    }
+    public class QREncoder
+    {
+        public QREncodingMode DetermineEncodingMode(string data)
+        {
+            if (IsNumeric(data))
+            {
+                return QREncodingMode.Numeric;
+            }
+            else if (IsAlphanumeric(data))
+            {
+                return QREncodingMode.Alphanumeric;
+            }
+            else if (IsByte(data))
+            {
+                return QREncodingMode.Byte;
+            }
+            else if (IsKanji(data))
+            {
+                return QREncodingMode.Kanji;
+            }
+
+            return QREncodingMode.Unknown;
+        }
+
+        private bool IsNumeric(string data)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(data, "^[0-9]+$");
+        }
+
+        private bool IsAlphanumeric(string data)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(data, "^[A-Z0-9 $%*+-./:]*$");
+        }
+
+        private bool IsByte(string data)
+        {
+            // For simplicity, we assume ISO-8859-1 character set
+            return System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(data).Length == data.Length;
+        }
+
+        private bool IsKanji(string data)
+        {
+            // For simplicity, we don't implement a real check for Kanji characters
+            // You may need to implement a more sophisticated check based on the Shift JIS character set
+            return false;
+        }
+    }
+}
